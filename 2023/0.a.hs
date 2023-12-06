d7fa01b99dda50626b81a03aa371adde6dbb7480
nif <romanstirrat@gmail.com>
2023-12-06 22:42:57 +0000

    more sane order

--- 2023/0.a.hs
+++ 2023/0.a.hs
@@ -1,14 +1,14 @@
 import System.IO
 import Data.Char
 
+toInt :: Char -> Int
+toInt x | isDigit x = ord x - ord '0'
+
+f :: String -> Int
+f = (\xs -> toInt (head xs) * 10 + toInt (last xs)) . filter isDigit
+
 main :: IO ()
 main = openFile "0.i.txt" ReadMode >>= \handle -> 
        hGetContents handle >>= \contents ->
        print (sum (map f (words contents))) >>
        hClose handle
-
-f :: String -> Int
-f = (\xs -> toInt (head xs) * 10 + toInt (last xs)) . filter isDigit
-
-toInt :: Char -> Int
-toInt x | isDigit x = ord x - ord '0'
