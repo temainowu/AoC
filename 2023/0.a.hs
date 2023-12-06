759f3db3849090acddc2201523cbe452da1123be
nif <romanstirrat@gmail.com>
2023-12-06 22:41:31 +0000

    part a done

---
+++ 2023/0.a.hs
@@ -0,0 +1,15 @@
+import System.IO
+import Control.Monad
+import Data.Char
+
+main :: IO ()
+main = openFile "0.i.txt" ReadMode >>= \handle -> 
+       hGetContents handle >>= \contents ->
+       print (sum (map f (words contents))) >>
+       hClose handle
+
+f :: String -> Int
+f = (\xs -> toInt (head xs) * 10 + toInt (last xs)) . filter isDigit
+
+toInt :: Char -> Int
+toInt x | isDigit x = ord x - ord '0'
